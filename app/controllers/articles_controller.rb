be059218adcc135083c45c991ca8245b5c6e7f5e
Kirk Paradis <kirk.paradis@gmail.com>
2021-02-04 14:11:12 -0500

    Added article listing page and feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+  end
+
 end
+
+# We are using @ to make instacne variables so that they can be accessed by the views.
