e19143dafc66acba1601d33c40609e1023ab8898
Kirk Paradis <kirk.paradis@gmail.com>
2021-02-05 10:42:55 -0500

    refactored methods in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -13,12 +13,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params_whitelist)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
     else
@@ -27,7 +25,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params_whitelist)
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
@@ -37,9 +35,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params_whitelist
+    params.require(:article).permit(:title, :description)
   end
 end
 
