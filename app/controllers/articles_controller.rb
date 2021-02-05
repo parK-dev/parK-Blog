26c9974d8bbcc24ff233c52a6c38006c7f02a289
Kirk Paradis <kirk.paradis@gmail.com>
2021-02-05 09:52:56 -0500

    Added the edit article feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,20 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
