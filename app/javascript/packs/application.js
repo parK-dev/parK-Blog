2eda861e8015b486ef2e78db4f9a93425dc9e4c1
Kirk Paradis <kirk.paradis@gmail.com>
2021-02-05 23:59:56 -0500

    Fixed the issue with the toggling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
+require ("packs/custom")
+require("packs/posts.js")
 
 Rails.start()
 Turbolinks.start()
