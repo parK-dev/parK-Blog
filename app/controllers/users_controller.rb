48ceb0546abd8a13bade01329297c66f43eea576
Kirk Paradis <kirk.paradis@gmail.com>
2021-02-21 15:42:48 -0500

    Quick styling fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params_whitelist)
       flash[:notice] = "Your profile was successfully updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
