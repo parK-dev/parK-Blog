a3472f6e8505a4522e3b6e81c038765112446c16
Kirk Paradis <kirk.paradis@gmail.com>
2021-02-24 09:21:08 -0500

    Added test for reject category

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -12,4 +12,16 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "test", response.body
   end
+
+  test "get new category form and reject category submission" do
+    get new_category_path
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: ""} }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
+
 end
