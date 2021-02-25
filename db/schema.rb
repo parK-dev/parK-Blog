760161124682213d11c119469b9d454af0bad572
Kirk Paradis <kirk.paradis@gmail.com>
2021-02-25 14:51:26 -0500

    Added many-to-many articles/categories backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_190836) do
+ActiveRecord::Schema.define(version: 2021_02_25_194214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
